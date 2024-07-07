cf360c2d73a277c0ca98b77cea8c9bf72405faae
Michal Goryszewski <goryszewski@gmail.com>
2024-07-07 11:58:43 +0200

    Func GetNodeByIp

--- libvirtApiClient/nodes.go
+++ libvirtApiClient/nodes.go
@@ -4,8 +4,30 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strings"
 )
+
+func (c *Client) GetNodeByIp(ip string) (*NodeV2, error) {
+	var url string = fmt.Sprintf("%s/api/v2/metadata", c.HostURL)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("problem wih NewRequest: %v", err.Error())
+	}
+
+	body, _, err := c.doRequest(req)
+	if err != nil {
+		return nil, fmt.Errorf("problem with doRequest: %v", err.Error())
+	}
+
+	var node NodeV2
+
+	err = json.Unmarshal(body, &node)
+	if err != nil {
+		return nil, fmt.Errorf("problem with Unmarshal: %v", err.Error())
+	}
+
+	return &node, nil
+
+}
 
 func (c *Client) GetNodeByName(name string) (*NodeV2, error) {
 
@@ -50,33 +72,4 @@ func (c *Client) GetNodes() (*[]NodeV2, error) {
 	}
 
 	return &nodes, nil
-}
-
-// GetIPByNodeName [deprecated]
-func (c *Client) GetIPByNodeName(nodename string) (*Worker, error) { // DEP
-
-	payload, err := json.Marshal(map[string]string{"function": "GetNodeByHostname", "hostname": nodename})
-	if err != nil {
-		return &Worker{}, err
-	}
-	request, err := http.NewRequest("POST", fmt.Sprintf("%v/api/v1/k8s/node", c.HostURL), strings.NewReader(string(payload)))
-	if err != nil {
-		return &Worker{}, err
-	}
-
-	body, _, err := c.doRequest(request)
-	if err != nil {
-		return &Worker{}, err
-	}
-
-	var node Worker
-
-	err = json.Unmarshal(body, &node)
-
-	if err != nil {
-		return &Worker{}, err
-	}
-
-	return &node, nil
-
 }
